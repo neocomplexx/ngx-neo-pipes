563021c5f9afc521d97c5beb887938b0dcc8999a
Sebastian Larrieu <slarrieu@team.neocomplexx.com>
2018-11-20 12:04:46 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'neo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'neo-pipes';
+}
