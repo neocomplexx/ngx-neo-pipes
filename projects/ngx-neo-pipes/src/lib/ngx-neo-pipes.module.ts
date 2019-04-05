b2bff598ee9108bb6a0807ea80484c59a60420ab
Sebastian Larrieu <slarrieu@team.neocomplexx.com>
2019-04-05 11:31:33 -0300

    Fix in export pipe. New NPM version 7.0.2

--- projects/ngx-neo-pipes/src/lib/ngx-neo-pipes.module.ts
+++ projects/ngx-neo-pipes/src/lib/ngx-neo-pipes.module.ts
@@ -1,14 +1,14 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import {
   NumberPipe, TextPipe, TextPipeTwoLevel, NumberPipeTwoLevel,
-  BeautifyPipe, DatePipe, RangePipe, RangePipeTwoLevel
+  BeautifyPipe, DatePipe, RangePipe, RangePipeTwoLevel, ToStringPipe
 } from './filters.pipe';
 import { PadLeftPipe } from './number.pipe';
 
 @NgModule({
-  declarations: [NumberPipe, TextPipe, TextPipeTwoLevel, NumberPipeTwoLevel,
+  declarations: [NumberPipe, TextPipe, TextPipeTwoLevel, NumberPipeTwoLevel, ToStringPipe,
     BeautifyPipe, DatePipe, RangePipe, RangePipeTwoLevel, PadLeftPipe],
-  exports: [NumberPipe, TextPipe, TextPipeTwoLevel, NumberPipeTwoLevel,
+  exports: [NumberPipe, TextPipe, TextPipeTwoLevel, NumberPipeTwoLevel, ToStringPipe,
     BeautifyPipe, DatePipe, RangePipe, RangePipeTwoLevel, PadLeftPipe],
 })
 export class NgxNeoPipesModule {
